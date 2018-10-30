60e45bf081dc0d2e5b086202db1e48fe9e5b768a
bivi325 <bivi325@gmail.com>
2018-10-30 21:13:19 +0200

    Added function doubleNumbers and sortByWeight

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,15 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const stringNum = num.toString();
+  const length = stringNum.length;
+
+  if (length % 2 === 0){
+    const leftString = stringNum.slice(0, length / 2);
+    const rightString = stringNum.slice(length / 2);
+    if (leftString === rightString){
+      return num;
+    }
+  }
+
+  return num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const compare = (a, b) => a.value - b.value;
+  const maperToObj = el => {
+    return { key: el, value: el.split('').reduce((a,b) => Number(a) + Number(b)) };
+  };
+  const result = arr.map(maperToObj).sort(compare).map(el => el.key);
+  return result;
 };
